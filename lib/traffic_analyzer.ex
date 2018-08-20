856d27e99813e351e62e023b9421c4df3a2554e7
Sergio Gernyak <sergeg1990@gmail.com>
2018-08-20 10:08:58 +0300

    ✨😱 Initial commit

---
+++ lib/traffic_analyzer.ex
@@ -0,0 +1,18 @@
+defmodule TrafficAnalyzer do
+  @moduledoc """
+  Documentation for TrafficAnalyzer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrafficAnalyzer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/traffic_analyzer_test.exs
@@ -0,0 +1,8 @@
+defmodule TrafficAnalyzerTest do
+  use ExUnit.Case
+  doctest TrafficAnalyzer
+
+  test "greets the world" do
+    assert TrafficAnalyzer.hello() == :world
+  end
+end
