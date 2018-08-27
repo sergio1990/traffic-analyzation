8cb7b9e6170c61e8d8864b74748a11f8ae167ee0
Sergio Gernyak <sergeg1990@gmail.com>
2018-08-27 11:47:10 +0300

    Approach to store keys outside repo

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
+
 config :logger,
   backends: [:console],
   level: :debug
@@ -16,7 +17,7 @@ config :traffic_analyzer,
 
 config :exq,
   name: Exq,
-  host: "127.0.0.1",
+  host: "<REDIS-HOST>",
   port: 6379,
   namespace: "exq",
   concurrency: :infinite,
@@ -28,30 +29,6 @@ config :exq,
   shutdown_timeout: 5000
 
 config :google_maps,
-  api_key: "<API_KEY>"
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+  api_key: "<API-KEY>"
 
-# You can configure your application as:
-#
-#     config :traffic_analyzer, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:traffic_analyzer, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env()}.exs"
+import_config "#{Mix.env()}.secret.exs"
---
+++ config/example.secret.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :exq,
+  host: "<REDIS-HOST>"
+
+config :google_maps,
+  api_key: "<API-KEY>"
