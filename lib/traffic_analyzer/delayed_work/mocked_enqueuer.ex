b56c1c2c9f5d437d1d40ab6c0700ae307e12c09f
Sergio Gernyak <sergeg1990@gmail.com>
2018-08-31 13:13:29 +0300

    Add MockedEnqueuer and use it in test env

---
+++ lib/traffic_analyzer/delayed_work/mocked_enqueuer.ex
@@ -0,0 +1,13 @@
+require Logger
+
+defmodule TrafficAnalyzer.DelayedWork.MockedEnqueuer do
+  alias TrafficAnalyzer.DelayedWork.JobEnqueuer
+
+  @behaviour JobEnqueuer
+
+  def enqueue(job_module, args) do
+    Logger.debug(fn ->
+      "Attempt to enqueue the job: #{job_module} with args #{inspect(args)}"
+    end)
+  end
+end
--- test/traffic_analyzer/path_source/db_source_test.exs
+++ test/traffic_analyzer/path_source/db_source_test.exs
@@ -1,13 +1,14 @@
 defmodule TrafficAnalyzer.PathSourceDBSourceTest do
   use TestCaseWithDbSandbox
 
-  alias TrafficAnalyzer.PathSource.{DBSource, Path}
+  alias TrafficAnalyzer.PathSource.DBSource
+  alias TrafficAnalyzer.PathData
 
   test "observable_paths returns all paths contained in the DB" do
     insert(:path)
     paths = DBSource.observable_paths()
     assert Enum.count(paths) == 1
     first_path = Enum.at(paths, 0)
-    assert %Path{} = first_path
+    assert %PathData{} = first_path
   end
 end
